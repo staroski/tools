d0c24fcdcdefd1b74c45156d41e52e3af87870b9
Ricardo Artur Staroski <ricardo.staroski@gmail.com>
2014-07-11 09:47:21 -0300

    Included the 'Command' tool

---
+++ src/br/com/staroski/tools/io/IO.java
@@ -0,0 +1,162 @@
+package br.com.staroski.tools.io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Classe utilitária para operações de I/O
+ * 
+ * @author Ricardo Artur Staroski
+ */
+public final class IO {
+
+	/**
+	 * Tamanho padrão, 8KB, utilizado para blocos de memória.
+	 */
+	public static int BLOCK_SIZE = 8192;
+
+	/**
+	 * Copia o arquivo de origem para o arquivo de destino.
+	 * 
+	 * @param from
+	 *            O arquivo de origem.
+	 * @param to
+	 *            O arquivo de destino.
+	 * @throws IOException
+	 */
+	public static void copy(File from, File to) throws IOException {
+		InputStream in = new FileInputStream(from);
+		OutputStream out = new FileOutputStream(to);
+		copy(in, out);
+		in.close();
+		out.close();
+		to.setLastModified(from.lastModified());
+	}
+
+	/**
+	 * Copia o conteúdo do stream de entrada para o stream de saída.
+	 * 
+	 * @param from
+	 *            O stream de entrada.
+	 * @param to
+	 *            O stream de saída.
+	 * @throws IOException
+	 */
+	public static void copy(InputStream from, OutputStream to) throws IOException {
+		final int count = BLOCK_SIZE;
+		byte[] bytes = new byte[count];
+		for (int read = -1; (read = from.read(bytes, 0, count)) != -1; to.write(bytes, 0, read))
+			;
+		to.flush();
+	}
+
+	/**
+	 * Copia o arquivo de origem para o arquivo de destino.
+	 * 
+	 * @param from
+	 *            O caminho do arquivo de origem.
+	 * @param to
+	 *            O caminho do arquivo de destino.
+	 * @throws IOException
+	 */
+	public static void copy(String from, String to) throws IOException {
+		copy(new FileInputStream(from), new FileOutputStream(to));
+	}
+
+	/**
+	 * Analisa o arquivo informado, se o mesmo não existir, um novo é criado
+	 * 
+	 * @param file
+	 *            O arquivo a ser verificado
+	 * @return O próprio parâmetro
+	 * @throws IOException
+	 */
+	public static File createIfNotExists(File file) throws IOException {
+		if (!file.exists()) {
+			File parent = file.getParentFile();
+			if (parent != null) {
+				parent.mkdirs();
+			}
+			file.createNewFile();
+		}
+		return file;
+	}
+
+	/**
+	 * Apaga o arquivo informado
+	 * 
+	 * @param file
+	 *            O arquivo a ser apagado
+	 * @throws IOException
+	 */
+	public static void delete(File file) throws IOException {
+		Files.deleteIfExists(file.toPath());
+	}
+
+	/**
+	 * Apaga o arquivo informado
+	 * 
+	 * @param file
+	 *            O arquivo a ser apagado
+	 * @throws IOException
+	 */
+	public static void delete(String file) throws IOException {
+		delete(new File(file));
+	}
+
+	/**
+	 * Obtém todas as linhas do arquivo informado
+	 * 
+	 * @param file
+	 *            O arquivo do qual se deseja ler as linhas
+	 * @return Uma lista de contendo as linhas do arquivo
+	 * @throws IOException
+	 */
+	public static List<String> readLines(File file) throws IOException {
+		List<String> lines = new ArrayList<String>();
+		BufferedReader input = new BufferedReader(new FileReader(file));
+		String line = null;
+		while ((line = input.readLine()) != null) {
+			lines.add(line);
+		}
+		input.close();
+		return lines;
+	}
+
+	/**
+	 * Grava as linhas no arquivo informado
+	 * 
+	 * @param lines
+	 *            As linhas a serem gravadas
+	 * @param file
+	 *            O arquivo no qual se deseja gravar as linhas
+	 * @return Uma lista de contendo as linhas do arquivo
+	 * @throws IOException
+	 */
+	public static void writeLines(File file, List<String> lines) throws IOException {
+		BufferedWriter output = new BufferedWriter(new FileWriter(file));
+		for (int i = 0, n = lines.size(); i < n; i++) {
+			if (i > 0) {
+				output.newLine();
+			}
+			output.write(lines.get(i));
+		}
+		output.flush();
+		output.close();
+	}
+
+	// não faz sentido instanciar esta classe
+	private IO() {
+	}
+}
---
+++ src/br/com/staroski/tools/runtime/Command.java
@@ -0,0 +1,218 @@
+package br.com.staroski.tools.runtime;
+
+
+import static br.com.staroski.tools.io.IO.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Classe utilitária para facilitar a execução de linhas de comando.<BR>
+ * Possui dois construtores:<BR>
+ * - o {@link #CommandLine(String, String...) primeiro} recebe a linha de comando e opcionalmente seus parâmetros;<BR>
+ * - o {@link #CommandLine(CommandListener, CommandListener, String, String...) segundo} aceita também dois {@link CommandListener listeners}, um para receber
+ * as mensagens da saída padrão do processo e outro para receber as mensagens da saída de erro. <BR>
+ * 
+ * @author Ricardo Artur Staroski
+ */
+public final class Command {
+
+	// classe utilizada internamente para armazenar as saídas padrão e de erro do processo
+	private static class EnchainedListener implements CommandListener {
+
+		public final StringBuilder buffer;
+
+		private final CommandListener enchained;
+
+		EnchainedListener(CommandListener listener) {
+			buffer = new StringBuilder();
+			enchained = listener == null ? NULL : listener;
+		}
+
+		@Override
+		public void receive(String text) {
+			buffer.append(text);
+			enchained.receive(text);
+		}
+	}
+
+	// classe interna utilizada para ler os stream de saida e de erro do processo
+	private class StreamReader implements Runnable {
+
+		private final InputStream stream;
+		private final CommandListener listener;
+
+		StreamReader(InputStream stream, CommandListener listener) {
+			this.stream = stream;
+			this.listener = listener;
+		}
+
+		@Override
+		public void run() {
+			try {
+				byte[] buffer = new byte[BLOCK_SIZE];
+				for (int read = -1; (read = stream.read(buffer)) != -1; listener.receive(new String(buffer, 0, read)))
+					;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	};
+
+	private final String[] command;
+	private final EnchainedListener outputListener;
+	private final EnchainedListener errorListener;
+
+	/**
+	 * Instancia uma nova linha de comando
+	 * 
+	 * @param output
+	 *            O objeto que recebera a saída padrão do processo
+	 * 
+	 * @param error
+	 *            O objeto que recebera a saída de erro do processo
+	 * 
+	 * @param exec
+	 *            O caminho do executável
+	 * @param params
+	 *            Opcional, os argumentos do executável
+	 */
+	public Command(CommandListener output, CommandListener error, String exec, String... params) {
+		outputListener = new EnchainedListener(output);
+		errorListener = new EnchainedListener(error);
+
+		int args = 1 + params.length;
+		command = new String[args];
+		command[0] = exec;
+		for (int i = 1; i < args; i++) {
+			command[i] = params[i - 1];
+		}
+	}
+
+	/**
+	 * Instancia uma nova linha de comando
+	 * 
+	 * @param exec
+	 *            O caminho do executável
+	 * @param params
+	 *            Opcional, os argumentos do executável
+	 */
+	public Command(String exec, String... params) {
+		this(null, null, exec, params);
+	}
+
+	/**
+	 * Executa esta linha de comando
+	 * 
+	 * @return O código de saída do processo
+	 * @throws IOException
+	 */
+	public int execute() throws IOException {
+		return execute(null);
+	}
+
+	/**
+	 * Executa esta linha de comando
+	 * 
+	 * @param directory
+	 *            O diretório a partir do qual a linha de comando será executada
+	 * 
+	 * @return O código de saída do processo
+	 * @throws IOException
+	 */
+	public int execute(File directory) throws IOException {
+		try {
+			Process process = executeAssinchronous(directory);
+			return process.waitFor();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
+
+	/**
+	 * Executa esta linha de comando assíncronamente, devolvendo o {@link Process processo} disparado, ficando a encardo do desenvolvedor invocar o método
+	 * {@link Process#waitFor() waitFor()} para obter o código de saída da aplicação
+	 * 
+	 * 
+	 * @return O {@link Process processo} que foi disparado
+	 * @throws IOException
+	 */
+	public Process executeAssinchronous() throws IOException {
+		return executeAssinchronous(null);
+	}
+
+	/**
+	 * Executa esta linha de comando assíncronamente, devolvendo o {@link Process processo} disparado, ficando a encardo do desenvolvedor invocar o método
+	 * {@link Process#waitFor() waitFor()} para obter o código de saída da aplicação
+	 * 
+	 * @param directory
+	 *            O diretório a partir do qual a linha de comando será executada
+	 * 
+	 * @return O {@link Process processo} que foi disparado
+	 * @throws IOException
+	 */
+	public Process executeAssinchronous(File directory) throws IOException {
+		ProcessBuilder builder = new ProcessBuilder(command);
+		builder.directory(directory);
+		Process process = builder.start();
+
+		Thread errorsReader = new Thread(new StreamReader(process.getErrorStream(), errorListener));
+		Thread outputReader = new Thread(new StreamReader(process.getInputStream(), outputListener));
+
+		errorsReader.start();
+		outputReader.start();
+
+		return process;
+	}
+
+	/**
+	 * @return o texto contendo as mensagens de erro do processo
+	 */
+	public String getError() {
+		return errorListener.buffer.toString();
+	}
+
+	/**
+	 * @return o texto contendo as mensagens de saída do processo
+	 */
+	public String getOutput() {
+		return outputListener.buffer.toString();
+	}
+
+	/**
+	 * Verifica se há mensagens de erro do processo
+	 * 
+	 * @return <code>true</code> se houver mensagem de erro <code>false</code> caso contrário
+	 */
+	public boolean hasError() {
+		return !getError().isEmpty();
+	}
+
+	/**
+	 * Verifica se há mensagens de saída do processo
+	 * 
+	 * @return <code>true</code> se houver mensagem de saída <code>false</code> caso contrário
+	 */
+	public boolean hasOutput() {
+		return !getOutput().isEmpty();
+	}
+
+	/**
+	 * Obtém o texto da linha de comando encapsulada
+	 * 
+	 * @return O texto da linha de comando encapsulada
+	 */
+	@Override
+	public String toString() {
+		StringBuilder text = new StringBuilder();
+		for (int i = 0, n = command.length; i < n; i++) {
+			if (i > 0) {
+				text.append(" ");
+			}
+			text.append(command[i]);
+		}
+		return text.toString();
+	}
+}
